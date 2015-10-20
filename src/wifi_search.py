c018ee586a9b6b8252132cc3b625a538761c0f04
Mitchell Lee <mitchell.t.lee@gmail.com>
2015-10-19 21:14:05 -0700

    Added CodeEval problem statement information to the wifi_search module

--- src/wifi_search.py
+++ src/wifi_search.py
@@ -1,6 +1,9 @@
 '''
 Functions that determine which buildings have Wi-Fi hotspots.
 
+The corresponding CodeEval challenge for this module is called "Where is Wi-Fi"
+(https://www.codeeval.com/open_challenges/159/).
+
 Created on Oct 19, 2015
 
 @author: Mitchell Lee
