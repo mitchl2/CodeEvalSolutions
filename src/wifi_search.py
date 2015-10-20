dc42fd03b82234767fe725d250af8b3ca828c8b7
Mitchell Lee <mitchell.t.lee@gmail.com>
2015-10-19 21:07:27 -0700

    Created empty src and test modules for the "Where is Wi-Fi" problem from CodeEval (https://www.codeeval.com/open_challenges/159/).

---
+++ src/wifi_search.py
@@ -0,0 +1,7 @@
+'''
+Functions that determine which buildings have Wi-Fi hotspots.
+
+Created on Oct 19, 2015
+
+@author: Mitchell Lee
+'''
---
+++ test/test_wifi_search.py
@@ -0,0 +1,7 @@
+'''
+Verifies the correct behavior of the wifi_search module
+
+Created on Oct 19, 2015
+
+@author: Mitchell Lee
+'''
